42b4c14034db069021a37cb4dae1ae69f50bf0c2
Sinan-muhammed <sinasinan418@gmail.com>
2025-09-08 20:07:17 +0530

    change in menu

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,184 +1,286 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState,useEffect } from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/List";
 import Collapse from "@mui/material/Collapse";
 import { Link } from "react-router-dom";
-import icon1 from '../../images/icon/call-calling.svg'
-import icon2 from '../../images/icon/icon-sms.svg'
-import menuimg from '../../images/gallery/burger-icon.png'
-
-
+import icon1 from "../../images/icon/call-calling.svg";
+import icon2 from "../../images/icon/icon-sms.svg";
+import menuimg from "../../images/gallery/burger-icon.png";
+import DecryptedText from "./decryptedText";
 
 const menus = [
-     {
-        id: 1,
-         title: 'Who we are NOT',
-        link: '/about',
-    },
-    // {
-    //     id: 3,
-    //     title: 'Who we are NOT',
-    //     link: '/about',
-    // },
-    // {
-    //     id: 4,
-    //     title: 'Analyse',
-    //     link: '/analyse',
-    // },
-    {
-        id: 5,
-        title: 'Consult Us',
-        link: '/Contact',
-    }
-    
-]
+  {
+    id: 1,
+    title: "Who we are NOT",
+    link: "/about",
+  },
+  
+  {
+    id: 5,
+    title: "Consult Us",
+    link: "/Contact",
+  },
+];
 const menus2 = [
-     {
-        id: 1,
-        title: 'Analyse',
-        link: '/analyse',
-    },
-    {
-        id: 2,
-        title: 'Govern',
-        link: '/govern',
-    },
-     {
-        id: 3,
-        title: 'Insure',
-        link: '/insure',
-    },
-    {
-        id: 4,
-        title: 'Defend',
-        link: '/defend',
-    },
-   
-]    
+  {
+    id: 1,
+    title: "Analyse",
+    link: "/analyse",
+  },
+  {
+    id: 2,
+    title: "Govern",
+    link: "/govern",
+  },
+  {
+    id: 3,
+    title: "Insure",
+    link: "/insure",
+  },
+  {
+    id: 4,
+    title: "Defend",
+    link: "/defend",
+  },
+];
 const menus3 = [
-     {
-        id: 1,
-        title: 'Hotline',
-        link: '/hotline',
-    }
-]
+  {
+    id: 1,
+    title: "Hotline",
+    link: "/hotline",
+  },
+];
 
 const MobileMenu = () => {
+    
+    
+  const [openId, setOpenId] = useState(0);
 
-    const [openId, setOpenId] = useState(0);
+  const ClickHandler = () => {
+    window.scrollTo(10, 0);
+  };
+  
+    
+   
 
-    const ClickHandler = () => {
-        window.scrollTo(10, 0);
-    }
-
-    return (
-        <div className='container'>
-        <ul className="clearfix xb-menu-primary">                          
-                                    <div className='menu-list-div'>
-                                        <div className='menu-sub-head'> <h1> Discover</h1></div>
-                                     <div className='menu-list-pages'>   
+  return (
+    <div className="container">
+      <ul className="clearfix xb-menu-primary">
+        <div className="menu-list-div">
+          <div className="menu-sub-head">
+            {" "}
+            <h1> Discover</h1>
+          </div>
+          <div className="menu-list-pages">
             {menus.map((item, mn) => {
-                return (
-                    <ListItem className={item.id === openId ? 'active' : null} key={mn}>
-                        {item.submenu ?
-                            <Fragment>
-                                <p onClick={() => setOpenId(item.id === openId ? 0 : item.id)}>{item.title}
-                                    <i className={item.id === openId ? 'fa fa-angle-up' : 'fa fa-angle-down'}></i>
-                                </p>
-                                <Collapse in={item.id === openId} timeout="auto" unmountOnExit>
-                                    <List className="menu-item menu-item-has-children active">
-                                        <Fragment>
-                                            {item.submenu.map((submenu, i) => {
-                                                return (
-                                                    <ListItem key={i}>
-                                                        <Link onClick={ClickHandler} className="active"
-                                                            to={submenu.link}>{submenu.title}</Link>
-                                                    </ListItem>
-                                                )
-                                            })}
-                                        </Fragment>
-                                    </List>
-                                </Collapse>
-                            </Fragment>
-                            : <Link className="active"
-                                to={item.link}>{item.title}</Link>
+              return (
+                <ListItem
+                  className={item.id === openId ? "active" : null}
+                  key={mn}
+                >
+                  {item.submenu ? (
+                    <Fragment>
+                      <p
+                        onClick={() =>
+                          setOpenId(item.id === openId ? 0 : item.id)
                         }
-                    </ListItem>
-                )
+                      >
+                        {item.title}
+                        <i
+                          className={
+                            item.id === openId
+                              ? "fa fa-angle-up"
+                              : "fa fa-angle-down"
+                          }
+                        ></i>
+                      </p>
+                      <Collapse
+                        in={item.id === openId}
+                        timeout="auto"
+                        unmountOnExit
+                      >
+                        <List className="menu-item menu-item-has-children active">
+                          <Fragment>
+                            {item.submenu.map((submenu, i) => {
+                              return (
+                                <ListItem key={i}>
+                                  <Link
+                                    onClick={ClickHandler}
+                                    className="active"
+                                    to={submenu.link}
+                                  >
+                                    {submenu.title}
+                                  </Link>
+                                </ListItem>
+                              );
+                            })}
+                          </Fragment>
+                        </List>
+                      </Collapse>
+                    </Fragment>
+                  ) : (
+                    <Link className="active" to={item.link}>
+                      {" "}
+                      <DecryptedText
+                        text={item.title}
+                        speed={70}
+                        maxIterations={10}
+                        characters="augmentity"
+                        className="revealed"
+                        parentClassName="all-letters"
+                        encryptedClassName="encrypted"
+                      />
+                    </Link>
+                  )}
+                </ListItem>
+              );
             })}
-            </div>
-            </div>
-                                    <div className='menu-list-div'>
-                                        <div className='menu-sub-head'> <h1> Services</h1></div>
-                                        <div className='menu-list-pages'>
-            {menus2.map((item, mn) => {
-                return (
-                    <ListItem className={item.id === openId ? 'active' : null} key={mn}>
-                        {item.submenu ?
-                            <Fragment>
-                                <p onClick={() => setOpenId(item.id === openId ? 0 : item.id)}>{item.title}
-                                    <i className={item.id === openId ? 'fa fa-angle-up' : 'fa fa-angle-down'}></i>
-                                </p>
-                                <Collapse in={item.id === openId} timeout="auto" unmountOnExit>
-                                    <List className="menu-item menu-item-has-children active">
-                                        <Fragment>
-                                            {item.submenu.map((submenu, i) => {
-                                                return (
-                                                    <ListItem key={i}>
-                                                        <Link onClick={ClickHandler} className="active"
-                                                            to={submenu.link}>{submenu.title}</Link>
-                                                    </ListItem>
-                                                )
-                                            })}
-                                        </Fragment>
-                                    </List>
-                                </Collapse>
-                            </Fragment>
-                            : <Link className="active"
-                                to={item.link}>{item.title}</Link>
-                        }
-                    </ListItem>
-                )
-            })}
-            </div>
-            </div>
-                                    <div className='menu-list-div'>
-                                         <div className='menu-sub-head'> <h1> Under attack?</h1></div>
-                                         <div className='menu-list-pages'>
-            {menus3.map((item, mn) => {
-                return (
-                    <ListItem className={item.id === openId ? 'active' : null} key={mn}>
-                        {item.submenu ?
-                            <Fragment>
-                                <p onClick={() => setOpenId(item.id === openId ? 0 : item.id)}>{item.title}
-                                    <i className={item.id === openId ? 'fa fa-angle-up' : 'fa fa-angle-down'}></i>
-                                </p>
-                                <Collapse in={item.id === openId} timeout="auto" unmountOnExit>
-                                    <List className="menu-item menu-item-has-children active">
-                                        <Fragment>
-                                            {item.submenu.map((submenu, i) => {
-                                                return (
-                                                    <ListItem key={i}>
-                                                        <Link onClick={ClickHandler} className="active"
-                                                            to={submenu.link}>{submenu.title}</Link>
-                                                    </ListItem>
-                                                )
-                                            })}
-                                        </Fragment>
-                                    </List>
-                                </Collapse>
-                            </Fragment>
-                            : <Link className="active"
-                                to={item.link}>{item.title}</Link>
-                        }
-                    </ListItem>
-                )
-            })}
-            </div>
-            </div>
-        </ul>
+          </div>
         </div>
-    )
-}
+        <div className="menu-list-div">
+          <div className="menu-sub-head">
+            {" "}
+            <h1> Services</h1>
+          </div>
+          <div className="menu-list-pages">
+            {menus2.map((item, mn) => {
+              return (
+                <ListItem
+                  className={item.id === openId ? "active" : null}
+                  key={mn}
+                >
+                  {item.submenu ? (
+                    <Fragment>
+                      <p
+                        onClick={() =>
+                          setOpenId(item.id === openId ? 0 : item.id)
+                        }
+                      >
+                        {item.title}
+                        <i
+                          className={
+                            item.id === openId
+                              ? "fa fa-angle-up"
+                              : "fa fa-angle-down"
+                          }
+                        ></i>
+                      </p>
+                      <Collapse
+                        in={item.id === openId}
+                        timeout="auto"
+                        unmountOnExit
+                      >
+                        <List className="menu-item menu-item-has-children active">
+                          <Fragment>
+                            {item.submenu.map((submenu, i) => {
+                              return (
+                                <ListItem key={i}>
+                                  <Link
+                                    onClick={ClickHandler}
+                                    className="active"
+                                    to={submenu.link}
+                                  >
+                                    {submenu.title}
+                                  </Link>
+                                </ListItem>
+                              );
+                            })}
+                          </Fragment>
+                        </List>
+                      </Collapse>
+                    </Fragment>
+                  ) : (
+                    <Link className="active" to={item.link}>
+                      {" "}
+                      <DecryptedText
+                        text={item.title}
+                        speed={70}
+                        maxIterations={10}
+                        characters="ABCD1234!?"
+                        className="revealed"
+                        parentClassName="all-letters"
+                        encryptedClassName="encrypted"
+                      />
+                    </Link>
+                  )}
+                </ListItem>
+              );
+            })}
+          </div>
+        </div>
+        <div className="menu-list-div">
+          <div className="menu-sub-head">
+            {" "}
+            <h1> Under attack?</h1>
+          </div>
+          <div className="menu-list-pages">
+            {menus3.map((item, mn) => {
+              return (
+                <ListItem
+                  className={item.id === openId ? "active" : null}
+                  key={mn}
+                >
+                  {item.submenu ? (
+                    <Fragment>
+                      <p
+                        onClick={() =>
+                          setOpenId(item.id === openId ? 0 : item.id)
+                        }
+                      >
+                        {item.title}
+                        <i
+                          className={
+                            item.id === openId
+                              ? "fa fa-angle-up"
+                              : "fa fa-angle-down"
+                          }
+                        ></i>
+                      </p>
+                      <Collapse
+                        in={item.id === openId}
+                        timeout="auto"
+                        unmountOnExit
+                      >
+                        <List className="menu-item menu-item-has-children active">
+                          <Fragment>
+                            {item.submenu.map((submenu, i) => {
+                              return (
+                                <ListItem key={i}>
+                                  <Link
+                                    onClick={ClickHandler}
+                                    className="active"
+                                    to={submenu.link}
+                                  >
+                                    {submenu.title}
+                                  </Link>
+                                </ListItem>
+                              );
+                            })}
+                          </Fragment>
+                        </List>
+                      </Collapse>
+                    </Fragment>
+                  ) : (
+                    <Link className="active" to={item.link}>
+                      <DecryptedText
+                        text={item.title}
+                        speed={70}
+                        maxIterations={10}
+                        characters="ABCD1234!?"
+                        className="revealed"
+                        parentClassName="all-letters"
+                        encryptedClassName="encrypted"
+                      />
+                    </Link>
+                  )}
+                </ListItem>
+              );
+            })}
+          </div>
+        </div>
+      </ul>
+    </div>
+  );
+};
 
 export default MobileMenu;
--- src/components/header4/Header4.js
+++ src/components/header4/Header4.js
@@ -1,108 +1,99 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { Fade } from "react-awesome-reveal";
 
-import logo from '../../images/logo/logo-png.png'
+import logo from '../../images/logo/logo-png.png';
+import MobileMenu from '../MobileMenu/MobileMenu';
 
-import MobileMenu from '../MobileMenu/MobileMenu'
+const Header4 = () => {
+  const [mobailActive, setMobailState] = useState(false);
+  const [isSticky, setSticky] = useState(false);
 
-const Header4 = (props) => {
+  const ClickHandler = () => {
+    window.scrollTo(10, 0);
+  };
 
-    const [mobailActive, setMobailState] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 80) {
+        setSticky(true);
+      } else {
+        setSticky(false);
+      }
+    };
 
-    const ClickHandler = () => {
-        window.scrollTo(10, 0);
-    }
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
-    const [isSticky, setSticky] = useState(false);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 80) {
-                setSticky(true);
-            } else {
-                setSticky(false);
-            }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-
-        // Clean up
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-
-    const SubmitHandler = (e) => {
-        e.preventDefault()
-    }
-
-    return (
-
-        <div id="xb-header-area" className="header-area header-style-three header-transparent">
-            <div className={`xb-header stricky  ${isSticky ? 'stricked-menu stricky-fixed' : ''}`}>
-                 
-                <div className="container">
-                    <div className="header__wrap ul_li_between">
-                        <div className="header-logo">
-                            <Link onClick={ClickHandler} to="/"><img src={logo} alt="" /></Link>
-                        </div>
-                        <hr className='header-hr'/>
-                        
-                        <div className="main-menu__wrap ul_li navbar navbar-expand-xl">
-                          
-                            <div className="xb-header-wrap">
-                                <div className={`xb-header-menu ${mobailActive ? "active" : ""}`}>
-                                    <div className="xb-header-menu-scroll lenis lenis-smooth">
-                                     
-                                        {/* <div className="xb-logo-mobile xb-hide-xl">
-                                            <Link onClick={ClickHandler} to="/" rel="home"><img src={logo}
-                                                alt="" /></Link>
-                                        </div> */}
-                                        <div className="xb-header-mobile-search xb-hide-xl">
-                                          
-                                        </div>
-                                        <nav className="xb-header-nav">
-                                            <MobileMenu />
-                                        </nav>
-                                    </div>
-                                </div>
-                                <div className="xb-header-menu-backdrop"></div>
-                            </div>
-                        </div>
-                        <div className="header-bar-mobile side-menu ">
-                            <button className="xb-nav-mobile">
-         
-    
-      <svg
-        className={`ham hamRotate ham3 ${mobailActive ? "active" : ""}`}
-        viewBox="0 0 100 100"
-        width="80"
-        onClick={() => setMobailState(!mobailActive)}
-      >
-  <path
-        class="line top"
-        d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40" />
-  <path
-        class="line middle"
-        d="m 30,50 h 40" />
-  <path
-        class="line bottom"
-        d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40" />
-</svg>
-               
-
-                            </button>
-                        </div>
-                       
-                    </div>
-                </div>
-                
+  return (
+    <header
+      id="xb-header-area"
+      className={`header-area header-style-three header-transparent ${
+        isSticky ? "is-sticky" : ""
+      }`}
+    >
+      <div className="xb-header">
+        <div className="container">
+          <div className="header__wrap ul_li_between">
+            {/* Logo */}
+            <div className="header-logo">
+              <Link onClick={ClickHandler} to="/">
+                <img src={logo} alt="Logo" />
+              </Link>
             </div>
-        </div>
 
-    )
-}
+            <hr className="header-hr" />
+
+            {/* Desktop Menu */}
+            <div className="main-menu__wrap ul_li navbar navbar-expand-xl">
+              <div className="xb-header-wrap">
+                <div className={`xb-header-menu ${mobailActive ? "active" : ""}`}>
+                  <div className="xb-header-menu-scroll lenis lenis-smooth">
+                    <nav className="xb-header-nav">
+                      <MobileMenu />
+                    </nav>
+                  </div>
+                </div>
+                <div className="xb-header-menu-backdrop"></div>
+              </div>
+            </div>
+
+            {/* Mobile Toggle Button */}
+            <div className="header-bar-mobile side-menu">
+              <button
+                className="xb-nav-mobile"
+                onClick={() => setMobailState(!mobailActive)}
+              >
+                <svg
+                  className={`ham hamRotate ham3 ${mobailActive ? "active" : ""}`}
+                  viewBox="0 0 100 100"
+                  width="80"
+                >
+                  <path
+                    className="line top"
+                    d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 
+                       0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 
+                       -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40"
+                  />
+                  <path className="line middle" d="m 30,50 h 40" />
+                  <path
+                    className="line bottom"
+                    d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 
+                       15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 
+                       -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 
+                       -18.435284,17.125656 l 0.252538,40"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header4;
