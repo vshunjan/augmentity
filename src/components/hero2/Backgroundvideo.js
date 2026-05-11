630194d69b519975fbb23452baa84b247ca3ccab
Sinan-muhammed <sinasinan418@gmail.com>
2026-05-12 00:39:14 +0530

    video autoplay fixed

--- src/components/hero2/Backgroundvideo.js
+++ src/components/hero2/Backgroundvideo.js
@@ -3,13 +3,20 @@ import React from "react";
 export default function BackgroundVideo() {
   return (
     <div className="video-container">
-     <iframe
-  src="https://player.mux.com/01QIelPYt01hh84998dCl00iBWMtVNhkWxOHrtP7qnZfC00?autoplay=1&muted=1&loop=1"
-  style={{ width: "100%", border: "none", aspectRatio: "16/9" }}
-  allow="autoplay; fullscreen; accelerometer; gyroscope; encrypted-media; picture-in-picture"
-  allowFullScreen
-  title="a"
-></iframe>
+   <iframe
+        src="https://player.mux.com/01QIelPYt01hh84998dCl00iBWMtVNhkWxOHrtP7qnZfC00?autoplay=muted&loop=true&playsinline=1"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+          aspectRatio: "16/9",
+          display: "block",
+        }}
+        loading="lazy"
+        allow="autoplay; fullscreen; accelerometer; gyroscope; encrypted-media; picture-in-picture"
+        allowFullScreen
+        title="About hero video"
+      />
     </div>
   );
 }
--- src/components/hero4/Backgroundvideo.js
+++ src/components/hero4/Backgroundvideo.js
@@ -4,7 +4,7 @@ export default function BackgroundVideo() {
   return (
     <div className="video-container index-page">
  <iframe
-  src="https://player.mux.com/rcaeAjdyzHffzOys9gH025XcBatj7j6wDGkcvqWwGHPM?autoplay=1&muted=true&loop=true&metadata-video-title=AdobeStock_1200676359_FHD&video-title=AdobeStock_1200676359_FHD&accent-color=%23040101"
+  src="https://player.mux.com/rcaeAjdyzHffzOys9gH025XcBatj7j6wDGkcvqWwGHPM?autoplay=muted&loop=true&playsinline=1"
   style={{ width: "100%", border: "none", aspectRatio: "16/9" }}
   allow="autoplay; fullscreen; accelerometer; gyroscope; encrypted-media; picture-in-picture"
   allowFullScreen
@@ -12,15 +12,13 @@ export default function BackgroundVideo() {
   className="desktop-vdo"
 />
 <iframe
-  src="https://player.mux.com/NqrRxGMICbSMVqKLo02qTKOE02oSTPaL3X01l5snA8fdx4?metadata-video-title=AdobeStock_1200676359_FHD+Vertical&video-title=AdobeStock_1200676359_FHD+Vertical&accent-color=%23040101&autoplay=1&muted=1&loop=1"
+  src="https://player.mux.com/NqrRxGMICbSMVqKLo02qTKOE02oSTPaL3X01l5snA8fdx4?autoplay=muted&loop=true&playsinline=1&metadata-video-title=AdobeStock_1200676359_FHD+Vertical&video-title=AdobeStock_1200676359_FHD+Vertical&accent-color=%23040101"
   style={{ width: "100%", border: "none", aspectRatio: "16/9" }}
   allow="autoplay; fullscreen; accelerometer; gyroscope; encrypted-media; picture-in-picture"
   allowFullScreen
   title="Mux Video"
   className="mobile-vdo"
 />
-
-
 
     </div>
   );
